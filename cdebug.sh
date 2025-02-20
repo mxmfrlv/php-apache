cc9e600cc944faf355375d4d9e7a2a4811c1458f
Maxim Frolov <maxim.frolov@univ-paris1.fr>
2025-02-20 02:09:02 +0100

    changing debugging image & shell

--- cdebug.sh
+++ cdebug.sh
@@ -17,7 +17,7 @@ fi
 
 ls -laF /proc/1/root/
 export PATH=$PATH:/.cdebug
-chroot /proc/1/root sh
+chroot /proc/1/root ash
 
 if [ $bin_created = True ] ; then
     rm /proc/1/root/bin
--- podman-debug.sh
+++ podman-debug.sh
@@ -2,5 +2,5 @@
 ### debugging slim container run under podman (analog to cdebug exec for docker)
 cont_name=${1:-php8}
 pod_name=${2:-pod_lamp_host}
-dbg_img_name=${3:-docker.io/library/busybox}
+dbg_img_name=${3:-docker.io/mxmfrlv/busybox-php-curl}
 podman run --rm -it --pod $pod_name --name debugger --pid container:$(podman container ps |grep $cont_name |head -1 |cut -d ' ' -f 1) -v ./cdebug.sh:/cdebug.sh $dbg_img_name sh -c "sh /cdebug.sh"
